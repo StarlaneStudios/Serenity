a94696ce98c1be104cec577a5a1eaaf90fdc29f2
Beau den Heijer <beau@yaacomm.com>
2023-08-18 16:08:15 +0200

    remove buttonVariant props overiding in docs

--- apps/docs/src/entry-client.tsx
+++ apps/docs/src/entry-client.tsx
@@ -1,8 +1,5 @@
 import { mount, StartClient } from "solid-start/entry-client";
 import "@serenity-ui/core/dist/style.css";
 import "./root.css";
-import { defaultButtonProps } from "@serenity-ui/core";
-
-defaultButtonProps.variant = "subtle"
 
 mount(() => <StartClient />, document);
