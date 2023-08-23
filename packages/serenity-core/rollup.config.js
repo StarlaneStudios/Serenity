7cb1c3e1e14200b1fba2625a0f051976fc1b7025
Beau den Heijer <beau@yaacomm.com>
2023-08-23 11:07:20 +0200

    feat: inject global mixins into core package

--- packages/serenity-core/rollup.config.js
+++ packages/serenity-core/rollup.config.js
@@ -1,5 +1,7 @@
 import withSolid from "rollup-preset-solid";
 import postcss from "rollup-plugin-postcss";
+import path from "path";
+import { fileURLToPath } from "url";
 
 export default withSolid({
 	input: "lib/index.tsx",
@@ -8,7 +10,12 @@ export default withSolid({
 	plugins: [
 		postcss({
 			extract: true,
-			use: ['sass'],
+			use: [
+				['sass', {
+					data: "@import '../serenity-styles/lib/sass/mixin.scss';",
+					includePaths: [path.join(fileURLToPath(import.meta.url), "lib")]
+				}]
+			],
 			minimize: false,
 			modules: true,
 			sourceMap: true
