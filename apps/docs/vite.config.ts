3bc475a2e930c73d65c3a55ad836ccb4c8172b93
Beau den Heijer <beau@yaacomm.com>
2023-08-24 10:13:31 +0200

    change: disable ssr for docs

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -2,21 +2,9 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	build: {
-		cssCodeSplit: true
-	},
 	plugins: [
 		solid({
-			ssr: true
+			ssr: false
 		})
-	],
-	ssr: {
-		noExternal: [
-			"@kobalte/core",
-			"@serenity-ui/core",
-			"solid-js",
-			"solid-js/web",
-			"solid-js/store"
-		]
-	}
+	]
 });
