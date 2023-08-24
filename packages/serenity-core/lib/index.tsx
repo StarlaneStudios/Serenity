96cee5e63b7aa40e580c4d66d5d3be473d6bf912
Julian Mills <julian.exodius@gmail.com>
2023-08-24 13:19:09 +0200

    refactor: clean up styles

--- packages/serenity-core/lib/index.tsx
+++ packages/serenity-core/lib/index.tsx
@@ -1,18 +1,13 @@
+import "./styles/variables.scss";
+import "./styles/global.scss";
+
 // components
 export * from "./components/accordion";
- 
 export * from "./components/button";
-
-export * from "./components/chevron/chevron";
-
-export * from "./components/divider/divider";
-
-export * from "./components/row/row";
-
-export * from "./components/paper/paper";
-
-export * from "./components/spacer/spacer";
-export * from "./components/layout/layout";
+export * from "./components/chevron";
 export * from "./components/column";
-
-import "./styles/root.css";
+export * from "./components/divider";
+export * from "./components/layout";
+export * from "./components/paper";
+export * from "./components/row";
+export * from "./components/spacer";
--- packages/serenity-core/rollup.config.js
+++ packages/serenity-core/rollup.config.js
@@ -1,7 +1,5 @@
 import withSolid from "rollup-preset-solid";
 import postcss from "rollup-plugin-postcss";
-import path from "path";
-import { fileURLToPath } from "url";
 
 export default withSolid({
 	input: "lib/index.tsx",
@@ -12,8 +10,7 @@ export default withSolid({
 			extract: true,
 			use: [
 				['sass', {
-					data: "@import '../serenity-styles/lib/sass/mixin.scss';",
-					includePaths: [path.join(fileURLToPath(import.meta.url), "lib")]
+					data: "@import '../serenity-styles/lib/sass/mixin.scss';"
 				}]
 			],
 			minimize: false,
