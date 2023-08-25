9ca3fd3be7d0dac0401ed4b657e252b4f47caf89
Beau den Heijer <beau@yaacomm.com>
2023-08-25 09:21:56 +0200

    feat: added variables component

--- packages/serenity-core/lib/components/variables/variables.tsx
+++ packages/serenity-core/lib/components/variables/variables.tsx
@@ -1,9 +1,9 @@
 import { ThemeScheme } from "@serenity-ui/styles";
-import { children, createEffect, mergeProps } from "solid-js";
+import { JSX, children, createEffect, mergeProps } from "solid-js";
 import { useTheme } from "../../providers/theme";
 
 interface VariablesProps {
-	children: any;
+	children: JSX.Element;
 	cssVariables: Record<string, any>;
 	cssThemeVariables: Record<
 		ThemeScheme | (string & {}),
@@ -15,25 +15,21 @@ function Variables(props: VariablesProps) {
 
 	const { theme } = useTheme();
 	const child = children(() => props.children) as unknown as () => HTMLElement;
-	// props.cssVariables, props.cssThemeVariables[theme()], child().style
 
 	const themeVariables = () => {
-		return props.cssThemeVariables[theme()];
-	};
-	
-	const styleProperties = () => {
-		const scheme = theme();
-		const themeVariables = props.cssThemeVariables[scheme];
-
-		return Object.assign(props.cssVariables, themeVariables);
+		return props.cssThemeVariables[theme()] ?? {};
 	};
 
-	return (
-		<pre>
-			{theme()}
-			{JSON.stringify(props.cssThemeVariables[theme()], null, 2)}
-		</pre>
-	)
+	createEffect(() => {
+
+		const variables = Object.assign(themeVariables(), props.cssVariables);
+
+		for (const [key, value] of Object.entries(variables)) {
+			child().style.setProperty(key, value);
+		}
+	});
+
+	return child();
 }
 
 export { Variables, VariablesProps };
