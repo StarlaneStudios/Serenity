1d8d71c765f6318387a4455d7630d105afe4d2f6
Julian Mills <julian.exodius@gmail.com>
2023-08-30 10:18:07 +0200

    fix: include root package.json in release commits

--- scripts/commit-versions.js
+++ scripts/commit-versions.js
@@ -2,5 +2,5 @@ import rootPkg from '../package.json' assert { type: 'json' }
 import { runAnyway } from './_helpers.js';
 
 runAnyway('git reset');
-runAnyway('git add ./packages/*/package.json');
+runAnyway('git add ./package.json ./packages/*/package.json');
 runAnyway(`git commit -m "release: version ${rootPkg.version}"`);
