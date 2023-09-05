c0f170b04068eec5e13237afc3de05237502b2cf
Julian Mills <julianyaacomm@gmail.com>
2023-09-05 16:04:13 +0200

    refactor: improve utility style props

--- packages/serenity-styles/lib/utilities.ts
+++ packages/serenity-styles/lib/utilities.ts
@@ -1,8 +1,11 @@
 import { type JSX, splitProps } from 'solid-js';
 import { parseLength } from './functions/values';
 import { UtilityStyleProps } from './types/props';
+import { $var } from './functions/theme/css';
 
-export const UTILITY_STYLE_PROPS = [
+type UtilityParsers = Partial<Record<keyof UtilityStyleProps, (arg: any) => string>>;
+
+const UTILITY_NAMES = [
 	'm', 'my', 'mx', 'mt', 'mb', 'mr', 'ml',
 	'p', 'py', 'px', 'pt', 'pb', 'pl', 'pr',
 	'bg',
@@ -18,53 +21,40 @@ export const UTILITY_STYLE_PROPS = [
 	'z',
 ] as const;
 
-function use<T, R>(value: T | undefined, parse?: (arg: T) => R): any {
-	return value === undefined ? undefined : (parse ? parse(value) : value);
-}
-
-function makeBaseStyle(utils: UtilityStyleProps): JSX.CSSProperties {
-	return {
-		'margin': use(utils.m, parseLength),
-		'margin-top': use(utils.mt, parseLength),
-		'margin-bottom': use(utils.mb, parseLength),
-		'margin-left': use(utils.ml, parseLength),
-		'margin-right': use(utils.mr, parseLength),
-		'margin-inline': use(utils.mx, parseLength),
-		'margin-block': use(utils.my, parseLength),
-		'padding': use(utils.p, parseLength),
-		'padding-top': use(utils.pt, parseLength),
-		'padding-bottom': use(utils.pb, parseLength),
-		'padding-left': use(utils.pl, parseLength),
-		'padding-right': use(utils.pr, parseLength),
-		'padding-inline': use(utils.px, parseLength),
-		'padding-block': use(utils.py, parseLength),
-		'background-color': use(utils.bg),
-		'color': use(utils.tc),
-		'width': use(utils.w, parseLength),
-		'min-width': use(utils.miw, parseLength),
-		'max-width': use(utils.maw, parseLength),
-		'height': use(utils.h, parseLength),
-		'min-height': use(utils.mih, parseLength),
-		'max-height': use(utils.mah, parseLength),
-		'position': utils.pos,
-		'top': use(utils.top, parseLength),
-		'left': use(utils.left, parseLength),
-		'bottom': use(utils.bottom, parseLength),
-		'right': use(utils.right, parseLength),
-		'inset': use(utils.inset, parseLength),
-		'flex': use(utils.flex),
-		'display': use(utils.display),
-		'cursor': use(utils.cursor),
-		'z-index': use(utils.z)
-	} satisfies JSX.CSSProperties;
-}
+const UTILITY_PARSERS: UtilityParsers  = {
+	'm': parseLength,
+	'mt': parseLength,
+	'mb': parseLength,
+	'ml': parseLength,
+	'mr': parseLength,
+	'mx': parseLength,
+	'my': parseLength,
+	'p': parseLength,
+	'pt': parseLength,
+	'pb': parseLength,
+	'pl': parseLength,
+	'pr': parseLength,
+	'px': parseLength,
+	'py': parseLength,
+	'w': parseLength,
+	'miw': parseLength,
+	'maw': parseLength,
+	'h': parseLength,
+	'mih': parseLength,
+	'mah': parseLength,
+	'top': parseLength,
+	'left': parseLength,
+	'bottom': parseLength,
+	'right': parseLength,
+	'inset': parseLength,
+};
 
 export type SplitPropsWithStyle<T, K extends (readonly (keyof T)[])[]> = [
 	JSX.CSSProperties,
     ...{
         [P in keyof K]: P extends `${number}` ? Pick<T, Extract<K[P], readonly (keyof T)[]>[number]> : never;
     },
-    Omit<T, K[number][number]>
+    Omit<T, K[number][number]> & { 'data-style': string }
 ];
 
 /**
@@ -78,7 +68,24 @@ export function splitPropsWithStyle<
 	T extends Record<any, any>,
 	K extends [readonly (keyof T)[], ...(readonly (keyof T)[])[]]
 >(props: T, ...keys: K): SplitPropsWithStyle<T, K> {
-	const [utils, ...rest] = splitProps(props as any, UTILITY_STYLE_PROPS, ...keys);
+	const [utils, ...rest] = splitProps(props, UTILITY_NAMES, ...keys);
+	const style: Record<string, string> = {};
+	const ids: string[] = [];
 
-	return [makeBaseStyle(utils), ...rest] as any;
+	for (const [key, value] of Object.entries(utils)) {
+		const parser = UTILITY_PARSERS[key as keyof UtilityParsers];
+		const parsed = parser ? parser(value) : value as string;
+		
+		style[`--serenity-util-${key}`] = parsed;
+
+		ids.push(key);
+	}
+
+	const attr = `-${ids.join('-')}-`;
+	const other = {
+		...rest.pop(),
+		'data-style': attr,
+	}
+
+	return [style, ...rest, other] as any;
 }
