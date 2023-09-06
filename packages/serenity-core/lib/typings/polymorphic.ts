ff09c01244ff4ddc8eba59d4078887b6ab986af0
Beau den Heijer <beau@yaacomm.com>
2023-09-06 10:34:36 +0200

    feat: added polymorphic types

--- packages/serenity-core/lib/typings/polymorphic.ts
+++ packages/serenity-core/lib/typings/polymorphic.ts
@@ -1,2 +1,17 @@
+import { SerenityBaseProps } from "@serenity-ui/styles";
 import type { Component, JSX, ComponentProps } from "solid-js";
+
 export type ElementType = keyof JSX.IntrinsicElements | Component<any>;
+
+export type PolymorphicProps<
+	TProps extends Object, 
+	TElement extends ElementType = ElementType
+> = {
+	as?: TElement;
+	children?: JSX.Element;
+} & Omit<ComponentProps<TElement>, keyof TProps> & SerenityBaseProps;
+
+export type PolymorphicUtilProps<
+	TProps extends Object, 
+	TElement extends ElementType = ElementType
+> = PolymorphicProps<TProps, TElement> & SerenityBaseProps;
