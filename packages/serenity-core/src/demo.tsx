64be132265195f7d6c6c3195da2ec78e00e8234d
Beau den Heijer <beau@yaacomm.com>
2023-09-11 15:39:50 +0200

    chore: updated demo page

--- packages/serenity-core/src/demo.tsx
+++ packages/serenity-core/src/demo.tsx
@@ -1,69 +1,46 @@
 import { mdiCheck, mdiLock } from "@mdi/js";
-import { Accordion, AccordionProps, Button, Column, Divider, Icon, InputField, Layout, Paper, Row, useSerenity } from "../lib";
+import { Accordion, AccordionProps, Button, Checkbox, Column, Divider, Icon, InputField, Layout, Paper, Row, useSerenity } from "../lib";
 import { Tabs } from "../lib/components/tabs";
 import { Badge } from "../lib/components/badge";
 import { Loader } from "../lib/components/loader";
-import { For, createEffect, createSignal, onMount } from "solid-js";
+import { For, JSX, createEffect, createSignal, onMount } from "solid-js";
 import { Text } from "../lib/components/text";
 import { Unit } from "../lib/components/unit";
 import { Alert } from "../lib/components/alert";
-import { createSign } from "crypto";
 import { Color, Variant, isColorLight } from "@serenity-ui/styles";
 
 export const DemoPage = () => {
 
 	const { toggleTheme } = useSerenity();
 	const [show, setShow] = createSignal(true);
-	const [color, setColor] = createSignal<Color>('blue.6');
 
-	const colors: Color[] = ["red", "blue", "indigo"];
-	const shades: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-	const generateColor = () => {
-		const randomColor = colors[Math.floor(Math.random() * colors.length)];
-		const randomShade = shades[Math.floor(Math.random() * shades.length)];
-
-		const color = [randomColor, randomShade].join('.') as Color;
-
-		setColor(color);
-	};
-
-	setInterval(generateColor, 2000);
-
-	const variants = () => {
-		const x = ['default', 'filled', 'outline', 'transparent', 'white', 'light'];
-
-		// fill array of length 100 with random variants
-		return Array.from({ length: 22 }, () => x[Math.floor(Math.random() * x.length)]);
+	const color = "blue.7";
+	const hideAlert: JSX.EventHandlerUnion<HTMLButtonElement, MouseEvent> = (event) => {
+		setShow(prev => !prev);
 	};
 
 	return (
 		<>
-			{isColorLight(color()) ? "Light" : "Dark"} <br />
-			{color()} <br />
 			<Row m={1}>
-				<Button onClick={() => setShow(prev => !prev)}>
+				<Button onClick={hideAlert}>
 					Click me
 				</Button>
 				<Button onclick={toggleTheme}>
 					Toggle theme
 				</Button>
-				<Button onclick={generateColor}>
-					Generate color
-				</Button>
 			</Row>
 			<Column p={1}>
-				<For 
-					each={variants() as any}
+				<For
+					each={['default', 'filled', 'outline', 'transparent', 'white', 'light'] as const}
 					fallback={<div>Failed</div>}
 				>
 					{(variant) => (
 						<Alert
 							variant={variant}
 							title="Dit is een alert"
-							onClose={(event) => setShow(false)}
+							onClose={hideAlert}
 							show={show()}
-							color={color()}
+							color={color}
 						>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit.
 							libero ducimus voluptatem natus,
@@ -72,10 +49,12 @@ export const DemoPage = () => {
 					)}
 				</For>
 			</Column>
-			<Loader />
-			<Badge variant="filled" color={color()}>
-				Mooi mooi
-			</Badge>
+			{/* <Loader /> */}
+			<Row>
+				<Checkbox ml="xl" label="Dit is een test" description="Why is there a checkbox?" />
+				<Checkbox color="grape" ml="xl" label="Dit is een test" description="Why is there a checkbox?" />
+				<Checkbox ml="xl" label="Dit is een test" description="Why is there a checkbox?" />
+			</Row>
 		</>
 	);
 };
