fc5beaabdd2dffab44bf530194824bd080a2670a
beau <beaudenheijer@gmail.com>
2023-09-20 22:35:47 +0200

    chore: update demo

--- packages/serenity-core/src/demo.tsx
+++ packages/serenity-core/src/demo.tsx
@@ -1,5 +1,5 @@
 import { mdiCheck, mdiLock } from "@mdi/js";
-import { Accordion, AccordionProps, Button, Checkbox, Column, Divider, Icon, InputField, Layout, Paper, Row, useSerenity } from "../lib";
+import { Accordion, AccordionProps, Button, Checkbox, Column, Divider, Entry, Icon, InputField, Layout, Paper, Row, useSerenity } from "../lib";
 import { Tabs } from "../lib/components/tabs";
 import { Badge } from "../lib/components/badge";
 import { Loader } from "../lib/components/loader";
@@ -8,6 +8,7 @@ import { Text } from "../lib/components/text";
 import { Unit } from "../lib/components/unit";
 import { Alert } from "../lib/components/alert";
 import { Color, Variant, isColorLight } from "@serenity-ui/styles";
+import classes from "./demo.module.scss";
 
 export const DemoPage = () => {
 
@@ -22,7 +23,7 @@ export const DemoPage = () => {
 	const onSubmit: JSX.EventHandlerUnion<HTMLFormElement, Event> = async (event) => {
 		event.preventDefault();
 		const form = new FormData(event.currentTarget);
-		
+
 		console.log(form.get('bruh'));
 	};
 
@@ -67,6 +68,18 @@ export const DemoPage = () => {
 					</button>
 				</Row>
 			</form>
+			<Entry
+				title="This is a title"
+				subtitle="This is a subtitle"
+				open
+				stylesStrategy="extend"
+				styles={{
+					chevron: classes['entry__chevron-test'],
+					inner: classes['entry__inner-test'],
+				}}
+			>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae suscipit cupiditate architecto iste facilis iure voluptatem. Fugit modi debitis voluptate, itaque recusandae, maiores inventore, voluptatum corrupti sit minus sapiente aliquam!	
+			</Entry>
 		</>
 	);
 };
--- packages/serenity-core/src/index.tsx
+++ packages/serenity-core/src/index.tsx
@@ -2,8 +2,6 @@ import { render } from "solid-js/web";
 import { DemoPage } from "./demo";
 import { SerenityProvider } from "../lib";
 
-import './demo.scss'
-
 render(() => (
 	<SerenityProvider
 		initialTheme="dark"
