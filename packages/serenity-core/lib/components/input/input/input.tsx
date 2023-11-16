6d04c2086c2702db35007673624108cf19ed0799
Beau den Heijer <beau@yaacomm.com>
2023-11-16 16:34:42 +0100

    fix: reactivity issue for error prop in input components

--- packages/serenity-core/lib/components/input/input/input.tsx
+++ packages/serenity-core/lib/components/input/input/input.tsx
@@ -1,4 +1,4 @@
-import { splitProps } from "solid-js";
+import { createEffect, createMemo, splitProps } from "solid-js";
 import { BaseInput, BaseInputProps, fieldInputSplitProps } from "../base";
 import { TextField as KobalteTextField } from "@kobalte/core";
 import createBlurInputTrigger from "../../../primitives/createBlurTrigger";
@@ -25,17 +25,17 @@ function InputField(props: InputFieldProps) {
 	const [input, extra, other] = splitProps(props, fieldInputSplitProps, splitBaseInputFieldProps);
 	const baseProps = mergeProps(defaultBaseInputFieldsProps, extra);
 
-	const blur = createBlurInputTrigger(other.value, baseProps.error, baseProps.lazyError);
+	const blur = createMemo(() => createBlurInputTrigger(other.value, baseProps.error, baseProps.lazyError));
 
 	return (
 		<BaseInput<InputFieldProps>
-			error={blur.value()}
+			error={blur().value()}
 			required={baseProps.required}
 			{...other}
 		>
 			<KobalteTextField.Input
 				type={extra.type}
-				onBlur={blur.handleBlur}
+				onBlur={blur().handleBlur}
 				{...input}
 			/>
 		</BaseInput>
