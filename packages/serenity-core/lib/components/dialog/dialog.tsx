3a3d3108bf82113718c93e664e00468769022e0a
Beau den Heijer <beau@yaacomm.com>
2024-01-11 16:25:33 +0100

    chore: initial work dialog component

--- packages/serenity-core/lib/components/dialog/dialog.tsx
+++ packages/serenity-core/lib/components/dialog/dialog.tsx
@@ -1,37 +1,72 @@
-import { JSX, mergeProps } from "solid-js";
-import { DefaultProps } from "../../typings/deprecated";
+import { mergeProps, splitProps } from "solid-js";
+import { Dialog as KobalteDialog } from "@kobalte/core";
+import { DefaultProps } from "../../typings/helpers";
+import classes from "./dialog.module.scss";
+import { resolveLength } from "../../utils/resolvers";
+import { localVars } from "../../utils/css";
+import { UTILITY_NAMES, buildStyles } from "../../utilities";
+import { SerenityBaseProps } from "../../typings/props";
+import { Length } from "../../typings/values";
 
-interface DialogProps extends JSX.DialogHtmlAttributes<HTMLDialogElement> {
+interface DialogProps extends SerenityBaseProps, KobalteDialog.DialogRootProps {
+	title: string;
+	description?: string;
 
+	blur?: number | string;
+	size?: Length;
 }
 
-const defaultDialogProps: DefaultProps<DialogProps, 'open'> = {
-	open: false
-};
+const defaultDialogProps = {
+	defaultOpen: false,
+	modal: true,
+	preventScroll: true,
+	blur: 5,
+	size: "lg"
+} satisfies Partial<DefaultProps<DialogProps>>;
+
+const dialogSplitProps = [
+	"defaultOpen",
+	"forceMount",
+	"id",
+	"modal",
+	"onOpenChange",
+	"open",
+	"preventScroll",
+	"blur",
+	"size"
+] satisfies (keyof DialogProps)[];
 
 function Dialog(props: DialogProps) {
 
-	const baseProps = mergeProps(defaultDialogProps, props);
+	const [root, utils, other] = splitProps(props, dialogSplitProps, UTILITY_NAMES);
+	const baseProps = mergeProps(defaultDialogProps, root);
 
-	let dialog: HTMLDialogElement;
-
-	const close = () => {
-		dialog.setAttribute('aria-hidden', 'true');
-		dialog.setAttribute('open', 'false');
+	const overlayCSSVariables = () => {
+		const blur = resolveLength("blur", baseProps.blur, "px");
+		return localVars({ blur });
 	};
 
-	const open = () => {
-		dialog.setAttribute('aria-hidden', 'false');
-		dialog.setAttribute('open', 'true');
-	};
+	const overlayStyles = () => buildStyles(utils, overlayCSSVariables());
 
 	return (
-		<dialog
-			open={baseProps.open}
-			aria-hidden={!baseProps.open}
+		<KobalteDialog.Root
+			{...root}
 		>
-			{baseProps.children}
-		</dialog>
+			<KobalteDialog.Trigger children={other.children} />
+			<KobalteDialog.Portal>
+				<KobalteDialog.Overlay
+					class={classes['dialog__overlay']}
+					{...overlayStyles()}
+				/>
+				<KobalteDialog.Content
+					class={classes['dialog__content']}
+				>
+					<KobalteDialog.CloseButton />
+					<KobalteDialog.Title children={props.title} />
+					<KobalteDialog.Description children={props.description} />
+				</KobalteDialog.Content>
+			</KobalteDialog.Portal>
+		</KobalteDialog.Root>
 	);
 }
 
